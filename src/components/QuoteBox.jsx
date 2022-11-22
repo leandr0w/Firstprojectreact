465e19812d47927d18652ce9a2b6a802d97e124c
Leandro Wasinger <leowasingerz@gmail.com>
2022-11-22 19:46:16 -0300

    First project react

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import BtnQuote from './BtnQuote'
+
+const QuoteBox = ({quoteRandom, handleClick, colorRandom}) => {
+    
+  const objStyle = {
+    color: colorRandom
+  } 
+  const objStyleBtn = {
+    backgroundColor: colorRandom
+  }
+  
+    return (
+    <article className='card' style={objStyle} >
+        <i className="card__icon fa-solid fa-quote-left"></i>
+        <p className='card__quote'>{quoteRandom.quote}</p>
+        <h1 className='card__autor'>{quoteRandom.author}</h1>
+        <BtnQuote
+          objStyleBtn={objStyleBtn}
+          handleClick={handleClick}
+        />
+    </article>
+  )
+}
+
+export default QuoteBox
